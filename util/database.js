fc2b686e359f2dcab6a324550e569d0016c01398
denyskarakycha <denys.karakycha@gmail.com>
2023-07-12 16:21:50 +0300

    Connected mongodb.

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require("sequelize");
+const e = require("express");
+const mongodb = require("mongodb");
 
-const sequelize = new Sequelize("node-complete", "root", "295q6722822", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://denys:295q6722822@cluster0.fk2cpgo.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = mongoConnect;
