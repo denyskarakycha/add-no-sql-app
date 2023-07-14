d1f0ea18bee9c7b987e14cccbb7f6544ba83d9cd
denyskarakycha <denys.karakycha@gmail.com>
2023-07-14 16:02:28 +0300

    Storing Multiple Products in the Cart.

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,11 +9,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
+  const userId = req.user._id;
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product(title, price, description, imageUrl, null, userId);
   product.save()
     .then(result => {
       console.log('Created Product');
--- models/user.js
+++ models/user.js
@@ -1,16 +1,53 @@
-const Sequelize = require('sequelize');
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
 
-const sequelize = require('../util/database');
+class User {
+  constructor (username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save () {
+    const db = getDb();
+    return db.collection('users').insertOne(this)
+     .then(result => {
+      console.log("User Created");
+     })
+     .catch(err => console.log(err))
+  }
+
+  addToCart (product) {
+    let updateCart;
+    if (!this.cart) {
+        updateCart = {items: [{productId: new mongodb.ObjectId(product._id), quantity: 1}]};
+    } else {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity = 1; 
+    const updateCartItems = [...this.cart.items];
+    
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;  
+      updateCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updateCartItems.push({productId: new mongodb.ObjectId(product._id), quantity: 1});
+    }
+
+    updateCart = {items: updateCartItems};
+  }
+    const db = getDb();
+    return db.collection('users').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: {cart: updateCart}})
+  }
+
+  static findById (userId) {
+    const db = getDb();
+    return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)});
+  }
+}
+
 
 module.exports = User;
